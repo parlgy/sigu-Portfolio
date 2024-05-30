338e8f2b01b8b43a9de49c91783256c96f1e9f0a
Parlgy <parlg4776@gmail.com>
2024-05-30 09:35:36 +0300

    update

--- src/pages/components/Book.jsx
+++ src/pages/components/Book.jsx
@@ -1,50 +1,10 @@
 // src/Book.js
 import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
-import styled from 'styled-components';
-import coverImage from '../../assets/images/cover.jpg'; // Add a cover image for realism
-import pageTexture from '../../assets/images/pageTexture.svg'; // Add a texture for pages
-
-const BookContainer = styled.div`
-  width: 100%;
-  height: 90vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #f0e5d8;
-`;
-
-const BookCover = styled.div`
-  background-image: url(${coverImage});
-  background-size: cover;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  font-size: 2em;
-  font-weight: bold;
-`;
-
-const Page = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: url(${pageTexture});
-  background-size: cover;
-  border: 1px solid #ccc;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-  padding: 20px;
-  font-size: 1.5em;
-  line-height: 1.6;
-`;
 
 const Book = () => {
   return (
-    <BookContainer>
+    <div className='book-container'>
       <HTMLFlipBook
         width={550}
         height={733}
@@ -56,16 +16,16 @@ const Book = () => {
         maxShadowOpacity={0.5}
         showCover={true}
       >
-        <BookCover className="page pt-20">
+        <div className="book-cover pt-10">
           <p>The Journey</p>
           <p>By Sigu</p>
-        </BookCover>
-        <Page className="page">This is page 1 of the book. You can add text, images, and other content here to make it look like a real book page.</Page>
-        <Page className="page">This is page 2 of the book. Continue adding content as needed.</Page>
-        <Page className="page">This is page 3 of the book.</Page>
-        <Page className="page">This is page 4 of the book.</Page>
+        </div>
+        <div className="page">This is page 1 of the book. You can add text, images, and other content here to make it look like a real book page.</div>
+        <div className="page">This is page 2 of the book. Continue adding content as needed.</div>
+        <div className="page">This is page 3 of the book.</div>
+        <div className="page">This is page 4 of the book.</div>
       </HTMLFlipBook>
-    </BookContainer>
+    </div>
   );
 };
 
