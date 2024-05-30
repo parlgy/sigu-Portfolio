aadda66363bd93f027ef7d8913eaf4bbb557855c
Parlgy <parlg4776@gmail.com>
2024-05-30 09:21:30 +0300

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Book from './components/Book'
+
+
+const Homepage = () => {
+    return (
+        <div className='App'>
+            <Book />
+        </div>
+    )
+}
+
+export default Homepage
